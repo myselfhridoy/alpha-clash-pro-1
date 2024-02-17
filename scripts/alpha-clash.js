51694141360e7c79eb1e2b2f89e2a9185516eede
hridoy65 <66057075+hridoy65@users.noreply.github.com>
2024-02-17 14:53:28 +0600

    Design Completed with some functions.

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,3 +1,25 @@
-function play(){
-    console.log("Start Play Now")
+// function play() {
+//     Step-1: Hide the home screen. To hide the screen add the class hidden to the home screen.
+//     Show the playground
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+
+//     const playGroundSection = document.getElementById('play-ground');
+//     playGroundSection.classList.remove('hidden');
+
+// }
+
+function continueGame(){
+    const alphabet = getARandomAlphabet();
+    console.log(alphabet);
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
+
+    setBackgroundColorById(alphabet);
+}
+
+function play() {
+    hideElementById('home-screen');
+    showElementById('play-ground');
+    continueGame();
 }
